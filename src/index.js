553d5ba0ae2ceb18f1e4995d26d011fff773a0bd
JKhrist <khrist@getonnet.agency>
2022-03-13 15:26:52 +0800

    completed setatrributes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,30 @@ registerBlockType('gutenblock/gutenblock-script', {
   title: 'Guten Block',
   icon: 'smiley',
   category: 'layout',
-  edit: ({ className }) => (
+
+  // custom attributes
+  attributes: {
+    author: {
+      type: 'string',
+    },
+  },
+  edit: ({ className, attributes, setAttributes }) => {
+    // custom functions
+    const updateAuthor = (event) => {
+      // update current attributes
+      setAttributes({ author: event.target.value });
+    };
+
+    return (
+      <div className={className}>
+        <h1>What is your name:</h1>
+        <input type='text' value={attributes.author} onChange={updateAuthor} />
+      </div>
+    );
+  },
+  save: ({ className, attributes }) => (
     <div className={className}>
-      <h1>Hello</h1>
-    </div>
-  ),
-  save: ({ className }) => (
-    <div className={className}>
-      <h1>Hi</h1>
+      <h1>Hi {attributes.author}</h1>
     </div>
   ),
 });

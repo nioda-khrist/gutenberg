f75e3fbd470ea44df54a368112dce22d29b4bc14
JKhrist <khrist@getonnet.agency>
2022-03-13 20:57:01 +0800

    use rich text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 registerBlockType('gutenblock/gutenblock-script', {
   title: 'Guten Block',
@@ -12,22 +13,41 @@ registerBlockType('gutenblock/gutenblock-script', {
     },
   },
   edit: ({ className, attributes, setAttributes }) => {
+    const { author } = attributes;
     // custom functions
-    const updateAuthor = (event) => {
+    const updateAuthor = (name) => {
       // update current attributes
-      setAttributes({ author: event.target.value });
+      setAttributes({ author: name });
     };
+
+    // using rich text
+    return (
+      <div className={className}>
+        <RichText
+          key='editable'
+          tagName='h2'
+          placeholder='Author Name'
+          value={author}
+          onChange={updateAuthor}
+        />
+      </div>
+    );
+
+    // using input
+    // return (
+    //   <div className={className}>
+    //     <h1>What is your name:</h1>
+    //     <input type='text' value={attributes.author} onChange={updateAuthor} />
+    //   </div>
+    // );
+  },
+  save: ({ className, attributes }) => {
+    const { author } = attributes;
 
     return (
       <div className={className}>
-        <h1>What is your name:</h1>
-        <input type='text' value={attributes.author} onChange={updateAuthor} />
+        <RichText.Content tagName='p' value={author} />
       </div>
     );
   },
-  save: ({ className, attributes }) => (
-    <div className={className}>
-      <h1>Hi {attributes.author}</h1>
-    </div>
-  ),
 });
